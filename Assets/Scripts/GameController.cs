aa9366973dc7621854d289b4cb89ebad6ef34a65
Rob Hailman <robhailman@gmail.com>
2016-08-27 12:23:23 -0400

    Basic two-camera setup
    Implemented the basic two-camera setup, for appropriate C64 goodness.

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+
+    public Material RenderTexture;
+    public Camera WorldCamera;
+
+    [Header("Resolution")]
+    public int TargetX = 160;
+    public int TargetY = 200;
+    private float pixelRatioAdjustment;
+
+    // Use this for initialization
+    void Start()
+    {
+        pixelRatioAdjustment = (float)TargetX / (float)TargetY;
+        if (pixelRatioAdjustment <= 1)
+        {
+            RenderTexture.mainTextureScale = new Vector2(pixelRatioAdjustment, 1);
+            RenderTexture.mainTextureOffset = new Vector2((1 - pixelRatioAdjustment) / 2, 0);
+            WorldCamera.orthographicSize = TargetY / 2;
+        }
+        else
+        {
+            pixelRatioAdjustment = 1f / pixelRatioAdjustment;
+            RenderTexture.mainTextureScale = new Vector2(1, pixelRatioAdjustment);
+            RenderTexture.mainTextureOffset = new Vector2(0, (1 - pixelRatioAdjustment) / 2);
+            WorldCamera.orthographicSize = TargetX / 2;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
