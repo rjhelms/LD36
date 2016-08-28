7f2bfd51ca12a01d5f77e6dc24c00a37ca8f855e
Rob Hailman <robhailman@gmail.com>
2016-08-27 23:35:45 -0400

    Add routine for generating river on level

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -1,5 +1,5 @@
 ﻿using UnityEngine;
-using System.Collections;
+
 
 enum RiverPiece
 {
@@ -20,6 +20,11 @@ public class LevelGenerator : MonoBehaviour
     public int StartY = -200;
     public int RiverWidth = 2;
     public int TileSize = 16;
+    public float RiverSinuosity = 0.2f;
+    public float RiverWidthChangeChance = 0.1f;
+
+    public int RiverMaxWidth = 6;
+    public int RiverMinWidth = 2;
 
     public Transform TerrainTransform;
 
@@ -50,10 +55,17 @@ public class LevelGenerator : MonoBehaviour
     {
         int currentY = 0;
         int currentX = 0;
-        float direction = Random.value - 0.5f;
+
+        int currentRowX = 0;
+        int currentRowWidth = 0;
+        int lastRowX = 0;
+        int lastRowWidth = 0;
+
+        // create river entry section
 
         int targetLeadingEdge = screenTileXSize / 2 + RiverWidth / 2;
-        if (direction < 0)
+        float direction = Random.value - 0.5f;
+        if (direction < 0.0f)
         {
             Debug.Log("River coming from left");
             Debug.Log("Target leading edge: " + targetLeadingEdge);
@@ -78,12 +90,14 @@ public class LevelGenerator : MonoBehaviour
                 }
                 currentX++;
                 currentY++;
+
             }
 
+            lastRowX = targetLeadingEdge - RiverWidth;
         }
         else
         {
-            currentX = 9;
+            currentX = screenTileXSize - 1;
             targetLeadingEdge = screenTileXSize - targetLeadingEdge;
             Debug.Log("River coming from right");
             Debug.Log("Target leading edge: " + targetLeadingEdge);
@@ -107,6 +121,181 @@ public class LevelGenerator : MonoBehaviour
                     tileX++;
                 }
                 currentX--;
+                currentY++;
+            }
+            lastRowX = targetLeadingEdge + 1;   // TODO: why plus one?
+        }
+
+        // generate main river section
+
+        lastRowWidth = RiverWidth;
+        while (currentY < targetLength)
+        {
+            // calculate shift
+            float changeRoll = Random.value;
+            if (changeRoll < RiverSinuosity)
+            {
+                float shiftDirectionRoll = Random.value;
+                if (shiftDirectionRoll < 0.5)
+                {
+                    currentRowX = lastRowX - 1;
+                }
+                else
+                {
+                    currentRowX = lastRowX + 1;
+                }
+
+            }
+            else { currentRowX = lastRowX; }
+
+            if (currentRowX < 0)
+                currentRowX = 0;
+            if ((currentRowX + lastRowWidth) >= screenTileXSize)
+                currentRowX = (screenTileXSize - lastRowWidth) - 1;
+
+            // calculate width change possibility, if hasn't shifted
+            if (RiverSinuosity < changeRoll && changeRoll < RiverWidthChangeChance)
+            {
+                float widthDirectionRoll = Random.value;
+
+                // bias to drift back to target width
+                if (lastRowWidth < RiverWidth)
+                    widthDirectionRoll *= 2;
+                if (lastRowWidth > RiverWidth)
+                    widthDirectionRoll /= 2;
+
+                if (widthDirectionRoll < 0.5)
+                {
+                    currentRowWidth = lastRowWidth - 1;
+                }
+                else
+                {
+                    currentRowWidth = lastRowWidth + 1;
+                }
+
+                if (currentRowWidth < RiverMinWidth)
+                    currentRowWidth = RiverMinWidth;
+                if (currentRowWidth > RiverMaxWidth)
+                    currentRowWidth = RiverMaxWidth;
+            }
+            else
+            {
+                currentRowWidth = lastRowWidth;
+            }
+            // hack to stop river from spilling off right size
+            if (currentRowX + currentRowWidth == screenTileXSize)
+                currentRowWidth -= 1;
+
+            // instantiate thingies
+            Debug.Log("CurrentY: " + currentY);
+            Debug.Log("CurrentRowX: " + currentRowX);
+            Debug.Log("CurrentRowWidth: " + currentRowWidth);
+            // create left bank
+            if (currentRowX < lastRowX)
+            {
+                Instantiate(RiverPrefabs[(int)RiverPiece.SE], TileCoordToPosition(currentRowX, currentY), Quaternion.identity, TerrainTransform);
+            }
+            else if (currentRowX > lastRowX)
+            {
+                Instantiate(RiverPrefabs[(int)RiverPiece.NE], TileCoordToPosition(currentRowX - 1, currentY), Quaternion.identity, TerrainTransform);
+                Instantiate(RiverPrefabs[(int)RiverPiece.FULL], TileCoordToPosition(currentRowX, currentY), Quaternion.identity, TerrainTransform);
+            }
+            else if (currentRowX == lastRowX)
+            {
+                Instantiate(RiverPrefabs[(int)RiverPiece.FULL], TileCoordToPosition(currentRowX, currentY), Quaternion.identity, TerrainTransform);
+            }
+            else
+            {
+                throw new System.InvalidOperationException("River generation broke the universe!");
+            }
+
+            // create right bank
+            int lastRowRightBank = lastRowX + lastRowWidth - 1;
+            int currentRowRightBank = currentRowX + currentRowWidth - 1;
+            Debug.Log("currentRowRightBank: " + currentRowRightBank);
+            if (currentRowRightBank < lastRowRightBank)
+            {
+                Instantiate(RiverPrefabs[(int)RiverPiece.NW], TileCoordToPosition(currentRowRightBank + 1, currentY), Quaternion.identity, TerrainTransform);
+                Instantiate(RiverPrefabs[(int)RiverPiece.FULL], TileCoordToPosition(currentRowRightBank, currentY), Quaternion.identity, TerrainTransform);
+            }
+            else if (currentRowRightBank > lastRowRightBank)
+            {
+                Instantiate(RiverPrefabs[(int)RiverPiece.SW], TileCoordToPosition(currentRowRightBank, currentY), Quaternion.identity, TerrainTransform);
+            }
+            else if (currentRowRightBank == lastRowRightBank)
+            {
+                Instantiate(RiverPrefabs[(int)RiverPiece.FULL], TileCoordToPosition(currentRowRightBank, currentY), Quaternion.identity, TerrainTransform);
+            }
+            else
+            {
+                throw new System.InvalidOperationException("River generation broke the universe!");
+            }
+            // fill in
+            for (int i = currentRowX + 1; i < currentRowRightBank; i++)
+            {
+                Instantiate(RiverPrefabs[(int)RiverPiece.FULL], TileCoordToPosition(i, currentY), Quaternion.identity, TerrainTransform);
+            }
+            // increment currentY and shift X and width variables
+            lastRowX = currentRowX;
+            lastRowWidth = currentRowWidth;
+            currentY++;
+        }
+
+        // generate river exit section
+        if (direction < 0.0f)
+        {
+            currentRowX += currentRowWidth;
+            Debug.Log("River leaving to the right");
+            Debug.Log("Target leading edge: " + targetLeadingEdge);
+            while (currentRowX - currentRowWidth < screenTileXSize)
+            {
+                int tileX = currentRowX;
+                while (Mathf.Abs(currentRowX - tileX) <= currentRowWidth)
+                {
+                    if (tileX == currentRowX)
+                    {
+                        Instantiate(RiverPrefabs[(int)RiverPiece.SW], TileCoordToPosition(tileX, currentY), Quaternion.identity, TerrainTransform);
+                    }
+                    else if (tileX != currentRowX && Mathf.Abs(currentRowX - tileX) < currentRowWidth)
+                    {
+                        Instantiate(RiverPrefabs[(int)RiverPiece.FULL], TileCoordToPosition(tileX, currentY), Quaternion.identity, TerrainTransform);
+                    }
+                    else
+                    {
+                        Instantiate(RiverPrefabs[(int)RiverPiece.NE], TileCoordToPosition(tileX, currentY), Quaternion.identity, TerrainTransform);
+                    }
+                    tileX--;
+                }
+                currentRowX++;
+                currentY++;
+
+            }
+        }
+        else
+        {
+            currentRowX--;
+            Debug.Log("River leaving to the left");
+            Debug.Log("Target leading edge: " + targetLeadingEdge);
+            while (currentRowX + currentRowWidth + 1 > 0) // TODO: why plus one?
+            {
+                int tileX = currentRowX;
+                while (Mathf.Abs(currentRowX - tileX) <= currentRowWidth)
+                {
+                    if (tileX == currentRowX)
+                    {
+                        Instantiate(RiverPrefabs[(int)RiverPiece.SE], TileCoordToPosition(tileX, currentY), Quaternion.identity, TerrainTransform);
+                    }
+                    else if (tileX != currentRowX && Mathf.Abs(currentRowX - tileX) < currentRowWidth)
+                    {
+                        Instantiate(RiverPrefabs[(int)RiverPiece.FULL], TileCoordToPosition(tileX, currentY), Quaternion.identity, TerrainTransform);
+                    }
+                    else
+                    {
+                        Instantiate(RiverPrefabs[(int)RiverPiece.NW], TileCoordToPosition(tileX, currentY), Quaternion.identity, TerrainTransform);
+                    }
+                    tileX++;
+                }
+                currentRowX--;
                 currentY++;
             }
         }
