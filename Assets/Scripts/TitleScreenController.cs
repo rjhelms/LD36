ad3728a96360147fb600db2902b3187e9eddb593
Rob Hailman <robhailman@gmail.com>
2016-08-28 20:44:51 -0400

    Add title screen

---
+++ Assets/Scripts/TitleScreenController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class TitleScreenController : MonoBehaviour {
+
+    public GameObject MusicPlayerPrefab;
+
+    // Use this for initialization
+    void Start () {
+        GameObject[] musicPlayers = GameObject.FindGameObjectsWithTag("Music");
+        if (musicPlayers.Length == 0)
+        {
+            GameObject musicPlayer = Instantiate(MusicPlayerPrefab);
+            DontDestroyOnLoad(musicPlayer);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            SceneManager.LoadScene("Main");
+        }
+    }
+}
